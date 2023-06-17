e21bab65ebbb81e073b865211e8204bb450f2dd1
tyler parker <tyler.parker@lan-party.com>
2023-06-17 12:25:59 -0600

    fix: testing still for amplify

--- apps/nextjs-test/app/components/app-footer/app-footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="mx-auto">
-            <p className="credits">Copyright &copy; 2023 tylercoder.com</p>
-        </footer>
-    )
-}
-
-export default Footer;
--- apps/nextjs-test/app/models/content-folders.ts
+++
@@ -1,3 +0,0 @@
-export const LINK_CONTENT_DIR:string = 'links';
-export const PROJECT_CONTENT_DIR:string = 'projects';
-export const CERT_CONTENT_DIR:string = 'certs';
