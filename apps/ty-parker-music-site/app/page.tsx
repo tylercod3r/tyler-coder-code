1b9de6b0672e85b8862dbb4bcbad93b5628a5e0e
tyler parker <tyler.parker@lan-party.com>
2023-06-20 20:27:30 -0600

    feat: ty-parker-music app created

---
+++ apps/ty-parker-music-site/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button, Header } from "ui";
+
+export default function Page() {
+  return (
+    <>
+      <Header text="Ty Parker Music - under construction" />
+      <Button />
+    </>
+  );
+}
