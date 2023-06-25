0702e2e9897ad03715710fe0cbe7657b2ee5c00a
tyler parker <tyler.parker@lan-party.com>
2023-06-25 01:12:43 -0600

    fix: cert section styling updated

--- apps/web/app/components/cert-library/cert-panel/index.tsx
+++ apps/web/app/components/cert-library/cert-panel/index.tsx
@@ -4,10 +4,11 @@ interface IProps {
 
 const CertPanel = (props: IProps) => {
   return (
-    <section className="p-5 m-4 border-2 border-slate-900">
+    <section className="p-5 m-4 border-2 rounded-lg border-slate-900">
       <h3>
         {props.cert.title.toUpperCase()}
       </h3>
+      <hr className="border-slate-600"></hr>
 
       <div>
         {props.cert.keywords?.map((keyword: string) => (
@@ -22,7 +23,7 @@ const CertPanel = (props: IProps) => {
           href={props.cert.urlCert}
           target="_blank"
           rel="noopener noreferrer"
-          className="mx-20 btn"
+          className="mx-20 btn btn-outline"
         >
           view cert
         </a>
