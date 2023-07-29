4feec2c5b3200dff4cf0f9add673d0c1ae7fa1eb
tyler parker <tyler.parker@lan-party.com>
2023-07-28 19:40:00 -0600

    fix: cbc header updated

--- apps/codebycandle-site/app-content/models/site-text.ts
+++ apps/codebycandle-site/app-content/models/site-text.ts
@@ -1,4 +1,4 @@
-export const siteTitle: string = "Tyler Coder";
+export const siteTitle: string = "Codebycandle";
 export const siteDescription: string =
   "Fullstack Web, Gaming, & XR." as string;
 
@@ -6,4 +6,4 @@ export const aboutLabel: string = "About";
 export const projectsLabel: string = "Projects";
 export const certsLabel: string = "Certifications";
 export const contactLabel: string = "Contact";
-export const contactEmail:string = "tyler@tylercoder.com";
+export const contactEmail:string = "contact@codebycandle.com";
